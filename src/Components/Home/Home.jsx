6cec3e91135ea49b0abc6d67afb8cb3ae357054d
ASSabbir <atiksabbir1125@gmail.com>
2025-01-06 05:14:02 +0600

    added some section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,9 @@ import Cars from './cars';
 import Extra from './Extra';
 import SpecialOffers from './SpecialOffers';
 
+
+
+
 const Home = () => {
     const {datas,dataloading}=useContext(AuthContext)
     console.log(dataloading)
